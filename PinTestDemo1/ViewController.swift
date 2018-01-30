d4079726c973db66ddb7510c4ee4454b7dd4eeed
WangJinling <wjlingOo@126.com>
2018-01-30 10:02:40 +0800

    Initial Commit

---
+++ PinTestDemo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinTestDemo1
+//
+//  Created by WangJinling on 2018/1/30.
+//  Copyright © 2018年 wjl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
